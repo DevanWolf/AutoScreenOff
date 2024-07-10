12967499c8383d7dbe03942e7c5ae48ab9be0b1c
Devan Wolf <8494722+DevanWolf@users.noreply.github.com>
2024-07-09 23:11:12 -0500

    Simplified

--- app/src/main/java/com/example/simplewakelock/MainActivity.kt
+++ app/src/main/java/com/example/simplewakelock/MainActivity.kt
@@ -8,16 +8,9 @@ import android.os.PowerManager
 import android.widget.Button
 import android.widget.TextView
 
-/**
- * Simple class used to demonstrate how to turn off screen
- * when proximity sensor senses something near him
- * (aka your head when making a call)
- */
 class MainActivity : AppCompatActivity() {
 
-    // Power manager will provide the lock
     private lateinit var powerManager: PowerManager
-    // The lock will handle himself the behaviour we want
     private lateinit var lock: PowerManager.WakeLock
 
     @SuppressLint("SetTextI18n")
@@ -28,38 +21,28 @@ class MainActivity : AppCompatActivity() {
         // Setting listeners on buttons
         findViewById<Button>(R.id.button_activate).setOnClickListener {
             enableBehaviour()
-            findViewById<TextView>(R.id.textview).text = "Behaviour Enabled"
+            findViewById<TextView>(R.id.textview).text = "On"
         }
         findViewById<Button>(R.id.button_disable).setOnClickListener {
             disableBehaviour()
-            findViewById<TextView>(R.id.textview).text = "Behaviour Disabled"
+            findViewById<TextView>(R.id.textview).text = "Off"
         }
 
-        // Declaring needed elements
         powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-        // Create a new wake lock relying on the proximity sensor
-        // You should make further research about that tag thing if you want to use other locks
-        lock = powerManager.newWakeLock(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,"simplewakelock:wakelocktag")
+        lock = powerManager.newWakeLock(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,"default")
     }
 
-    // Be sure to disable when exiting / pausing activity
-    // Otherwise it will continue even if your app is in background
     override fun onPause() {
         super.onPause()
         disableBehaviour()
     }
 
-    /* Note that i suppress the "WakelockTimeout" warning because i’m releasing it when i’m exiting the activity.
-    You could also acquire the lock for a predefined period of time. Ex :
-    lock.acquire(10*60*1000L /*10 minutes*/)   */
     @SuppressLint("WakelockTimeout")
     private fun enableBehaviour(){
-        // Acquire the lock if it was not already acquired
         if(!lock.isHeld) lock.acquire()
     }
 
     private fun disableBehaviour(){
-        // Release the lock if it was not already released
         if(lock.isHeld) lock.release()
     }
 }
